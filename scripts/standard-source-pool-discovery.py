c8d1ccf7326e9748f2fbb5a3331cefd3c6c6aaef
clientprogrammer-netizen <clientprogrammer@gmail.com>
2026-09-22 14:00:39 +0900

    research: fix standard source parser constants

--- scripts/standard-source-pool-discovery.py
+++ scripts/standard-source-pool-discovery.py
@@ -19,6 +19,8 @@ A_RE=re.compile(r'(?is)<a\b([^>]*?)href=["\']([^"\']+)["\']([^>]*)>(.*?)</a>')
 EMBED_RE=re.compile(r'(?is)<(?:iframe|video|source)\b([^>]*?)(?:src|data-src)=["\']([^"\']+)["\']([^>]*)>')
 TITLE_RE=re.compile(r'(?is)(?:title|aria-label)=["\']([^"\']+)["\']')
 ABS_URL_RE=re.compile(r'https?://[^\s"\'<>\\)]+', re.I)
+DDG_RESULT_RE=re.compile(r'(?is)<a[^>]+class=["\\']result__a["\\'][^>]+href=["\\']([^"\\']+)["\\'][^>]*>(.*?)</a>')
+HEADING_RE=re.compile(r'(?is)<h[1-6]\\b[^>]*>(.*?)</h[1-6]>')
 
 def fetch(url):
     req=urllib.request.Request(url,headers={"User-Agent":"ROPEJUMP-research/2.3 source-pool"})
